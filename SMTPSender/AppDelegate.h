38e3eb776faf32a35b548698ffa61d9c5b92acc0
wu xiaoming <wuxiaoming@wuxm.local>
2013-01-23 10:10:04 +0800

    Initial Commit

---
+++ SMTPSender/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SMTPSender
+//
+//  Created by wu xiaoming on 13-1-23.
+//  Copyright (c) 2013年 wu xiaoming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end

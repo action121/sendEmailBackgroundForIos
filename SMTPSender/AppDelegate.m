7407a2cd2a3a8c540d561593aa90d2cf4ec015e3
action121 <wuxiaoming@wuxiaomingtekiMacBook-Pro.local>
2013-12-17 17:28:28 +0800

    add

--- SMTPSender/AppDelegate.m
+++ SMTPSender/AppDelegate.m
@@ -7,8 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
-#import "ViewController.h"
+#import "SMTPSenderViewController.h"
 
 @implementation AppDelegate
 
@@ -19,13 +18,14 @@
     [super dealloc];
 }
 
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
-    // Override point for customization after application launch.
-    self.viewController = [[[ViewController alloc] initWithNibName:@"ViewController" bundle:nil] autorelease];
+    self.viewController = [[[SMTPSenderViewController alloc] initWithNibName:@"SMTPSenderViewController" bundle:nil] autorelease];
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
+
     return YES;
 }
 
